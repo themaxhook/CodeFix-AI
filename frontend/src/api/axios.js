4802c83a6c068e13efab3f1f580c710920057d3f
themaxhook <bharatchhabra703@gmail.com>
2026-05-14 01:07:39 +0530

    Implement-DockerFile

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://codefix-ai-yinx.onrender.com",
+  baseURL: "https://codefix-ai-yinx.onrender.com"||
+    "http://localhost:8080",
 });
 
 // automatically attach token to every request
